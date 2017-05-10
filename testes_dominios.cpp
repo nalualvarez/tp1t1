4a484f2ae7c8e642fadc813e2fb30365b1b1e32d
pelu97 <kalangoandrade@yahoo.com.br>
2017-05-10 17:11:29 -0300

    Testes de Dominios completos

--- testes_dominios.cpp
+++ testes_dominios.cpp
@@ -14,8 +14,12 @@ const static char TUEmail::arroba;
 const static char TUEmail::ponto;
 const static int TUAvaliacao::SUCESSO;
 const static int TUAvaliacao::FALHA;
+const static int TUAvaliacao::VALIDO;
+const static int TUAvaliacao::INVALIDO;
 const static int TUTexto::SUCESSO;
 const static int TUTexto::FALHA;
+const static int TUTexto::LIMITEVALIDO;
+const static int TUTexto::LIMITEINVALIDO;
 
 //definicoes de metodos da classe TUNome
 void TUNome::setUp(){
@@ -177,10 +181,96 @@ int TUEmail::run(){
     tearDown();
     return estado;
 }	
-		
-		
-		
-		
-		
-		
+
+//definicoes de metodos da classe TUAvaliacao
+
+void TUAvaliacao::setUp(){
+	avaliacao = new Avaliacao();
+	estado=SUCESSO;
+}
+
+void TUAvaliacao::tearDown{
+	delete avaliacao;
+}
+
+void TUAvaliacao::testarCenarioSucesso{	
+	try{
+		avaliacao->setAvaliacao(VALIDO);
+		if(avaliacao->getAvaliacao!=VALIDO){
+			estado=FALHA;
+		}
+	catch(invalid_argument excecao){
+		estado=FALHA;
+	}
+}
+
+void TUAvaliacao::testarCenarioFalha{
+	try{
+		avaliacao->setAvaliacao(INVALIDO);
+		estado=FALHA;
+	}
+	catch(invalid_argument excecao){
+		return;
+	}
+}
+
+int TUAvaliacao::run(){
+    setUp();
+    testarCenarioSucesso();
+    testarCenarioFalha();
+    tearDown();
+    return estado;
+}
+
+/*definicoes de metodos da clase TUTexto*/
+
+void TUTexto::setUp(){
+	int i;
+	
+	texto = new Texto();
+	for(i=0;i<LIMITEVALIDO;i++){
+		valido[i]='p';
+		invalido[i]='p';
+	}
+	valido[i]='\0';
+	for(i=50;i<LIMITEINVALIDO;i++){
+		invalido[i]='p';
+	}
+	invalido[i]='\0';
+	estado=SUCESSO;
+}
+
+void TUTexto::tearDown{
+	delete avaliacao;
+}
+
+void TUTexto::testarCenarioSucesso{	
+	try{
+		texto->setTexto(VALIDO);
+		if(texto->getTexto!=VALIDO){
+			estado=FALHA;
+		}
+	catch(invalid_argument excecao){
+		estado=FALHA;
+	}
+}
+
+void TUTexto::testarCenarioFalha{
+	try{
+		texto->setTexto(INVALIDO);
+		estado=FALHA;
+	}
+	catch(invalid_argument excecao){
+		return;
+	}
+}
+
+int TUTexto::run(){
+    setUp();
+    testarCenarioSucesso();
+    testarCenarioFalha();
+    tearDown();
+    return estado;
+}
+
 	
