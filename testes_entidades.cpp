e238ec58dd9693cb228b27ae0e052efac347795f
pelu97 <kalangoandrade@yahoo.com.br>
2017-05-10 19:14:34 -0300

    Primeiro teste de entidade pronto

--- testes_entidades.cpp
+++ testes_entidades.cpp
@@ -1,1 +1,27 @@
-#include
+#include "testes_entidades.h"
+
+const int SUCESSO;
+const int FALHA;
+
+//definicoes dos metodos da classe TUUser
+void TUUser::setUp(){
+	user = new User();
+	estado=SUCESSO;
+}
+
+int TUUser::run(){
+	int temp;
+	temp=teste_nome.run();
+	if (temp==FALHA){
+		estado=temp;
+	}
+	temp=teste_email.run();
+	if(temp==FALHA){
+		estado=temp;
+	}
+	temp=teste_senha.run();
+	if(temp==FALHA){
+		estado=temp;
+	}
+	return estado;
+}
--- testes_entidades.h
+++ testes_entidades.h
@@ -1,1 +1,23 @@
+#include "entidades.h"
+#include "testes_dominios.h"
 
+
+class TUUser{			//teste da classe do nome
+	private:
+		
+		TUNome teste_nome;
+		TUEmail teste_email;
+		TUSenha teste_senha;
+		
+		int estado;
+		
+		void setUp();					//metodos para executar o teste
+    	void tearDown();
+		
+	public:
+		
+		const static int SUCESSO = 0;		//constantes para executar o teste
+		const static int FALHA = -1;
+		
+		int run();
+};
